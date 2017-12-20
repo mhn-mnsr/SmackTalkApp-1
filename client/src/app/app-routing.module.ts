6c0ef68fdde5396f4afafc1542b46cd44caaaa60
4dar <adarbars@gmail.com>
2017-12-20 11:10:05 -0600

    fixed conflicts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './landing/login/login.component';
 import { RegisterComponent } from './landing/register/register.component';
 import { LandingComponent } from './landing/landing.component';
-<<<<<<< HEAD
 // import { SuccessComponent } from './success/success.component';
-=======
-import { CreateTeamComponent } from './choose-team/create-team/create-team.component';
-import { ChooseTeamComponent } from './choose-team/choose-team.component';
->>>>>>> 379781b82c9e6f6e7a952fbbbb54459f1ec1186b
 import { HomeComponent } from './home/home.component';
 
 
@@ -17,7 +12,6 @@ const routes: Routes = [
   { path: '', component: LandingComponent,children: [
       { path: '', component: RegisterComponent },
       { path: 'login', component: LoginComponent },
-<<<<<<< HEAD
     ]
   },
   // {
@@ -33,16 +27,6 @@ const routes: Routes = [
     children: [],
   },
 ];
-=======
-  ]},
-
-  { path: 'chooseTeam', component: ChooseTeamComponent, children: [
-    { path: 'createTeam', pathMatch: 'full', component: CreateTeamComponent},
-    { path: 'home', pathMatch: 'full', component: HomeComponent}]}
-]
-  
-
->>>>>>> 379781b82c9e6f6e7a952fbbbb54459f1ec1186b
 // export const routing = RouterModule.forRoot(routes);
 
 @NgModule({
